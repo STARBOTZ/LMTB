59bc48eaa0e0c54e95521e002b0a48a2c25c5042
STARBOTZ <starworldbot2@gmail.com>
2025-10-23 08:03:13 +0530

    Restrict broadcast command to bot owner
    Added OWNER_ID check to restrict broadcast command usage to the bot owner only.

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -6,7 +6,7 @@ from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
-from bot import bot, LOGGER, DATABASE_URL
+from bot import bot, LOGGER, DATABASE_URL, OWNER_ID  # ensure OWNER_ID is set in your bot __init__.py or config
 from bot.helper.ext_utils.db_handler import DbManger
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
 from bot.helper.telegram_helper.filters import CustomFilters
@@ -18,15 +18,23 @@ bc_cache = {}
 
 @new_task
 async def broadcast(_, message):
+    # 🔐 Owner-only restriction
+    if message.from_user.id != OWNER_ID:
+        return await sendMessage(message, "<b>Access Denied:</b> Only the bot owner can use this command!")
+
     bc_id, forwarded, quietly, deleted, edited = '', False, False, False, False
     if not DATABASE_URL:
         return await sendMessage(message, 'DATABASE_URL not provided!')
+
     rply = message.reply_to_message
     if len(message.command) > 1:
         if not message.command[1].startswith('-'):
             bc_id = message.command[1] if bc_cache.get(message.command[1], False) else ''
             if not bc_id:
-                return await sendMessage(message, "<i>Broadcast ID not found! After Restart, you can't edit or delete broadcasted messages...</i>")
+                return await sendMessage(
+                    message,
+                    "<i>Broadcast ID not found! After Restart, you can't edit or delete broadcasted messages...</i>"
+                )
         for arg in message.command:
             if arg in ['-f', '-forward'] and rply:
                 forwarded = True
@@ -36,6 +44,7 @@ async def broadcast(_, message):
                 deleted = True
             elif arg in ['-e', '-edit'] and bc_id and rply:
                 edited = True
+
     if not bc_id and not rply:
         return await sendMessage(message, '''<b>By replying to msg to Broadcast:</b>
 /broadcast bc_id -d -e -f -q
@@ -55,10 +64,12 @@ async def broadcast(_, message):
 <b>Notes:</b>
 1. Broadcast msgs can be only edited or deleted until restart.
 2. Forwarded msgs can't be Edited''')
+
     t, s, b, d, u = 0, 0, 0, 0, 0
+
     if deleted:
         temp_wait = await sendMessage(message, '<i>Deleting the Broadcasted Message! Please Wait ...</i>')
-        for msg in (msgs:=bc_cache[bc_id]):
+        for msg in (msgs := bc_cache[bc_id]):
             try:
                 await msg.delete()
                 await sleep(0.5)
@@ -73,6 +84,7 @@ async def broadcast(_, message):
 ┖ <b>Unsuccess Attempt:</b> <code>{u}</code>
 
 <b>Broadcast ID:</b> <code>{bc_id}</code>''')
+
     elif edited:
         temp_wait = await sendMessage(message, '<i>Editing the Broadcasted Message! Please Wait ...</i>')
         for msg in bc_cache[bc_id]:
@@ -94,6 +106,7 @@ async def broadcast(_, message):
 ┖ <b>Unsuccess Attempt:</b> <code>{u}</code>
 
 <b>Broadcast ID:</b> <code>{bc_id}</code>''')
+
     start_time = time()
     status = '''⌬  <b><i>Broadcast Stats :</i></b>
 ┠ <b>Total Users:</b> <code>{t}</code>
@@ -104,7 +117,9 @@ async def broadcast(_, message):
     updater = time()
     bc_hash, bc_msgs = str(uuid4()), []
     pls_wait = await sendMessage(message, status.format(**locals()))
+
     for uid in (await DbManger().get_pm_uids()):
+        bc_msg = None
         try:
             if forwarded:
                 bc_msg = await rply.forward(uid, disable_notification=quietly)
@@ -132,11 +147,12 @@ async def broadcast(_, message):
         if (time() - updater) > 10:
             await editMessage(pls_wait, status.format(**locals()))
             updater = time()
+
     bc_cache[bc_hash] = bc_msgs
     await editMessage(
         pls_wait,
         f"{status.format(**locals())}\n\n<b>Elapsed Time:</b> <code>{get_readable_time(time() - start_time)}</code>\n<b>Broadcast ID:</b> <code>{bc_hash}</code>",
     )
 
-
-bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.BroadcastCommand) & CustomFilters.sudo))
+# Only owner can trigger this command
+bot.add_handler(MessageHandler(broadcast, filters=command(BotCommands.BroadcastCommand)))
